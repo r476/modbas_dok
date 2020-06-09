be5fa975d9af23717186754c43159b0480f2dcc4
r476 <39871428+r476@users.noreply.github.com>
2020-06-09 08:01:54 +0700

    add files

---
+++ modbus_bot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import telebot
+token = '827576612:AAEX0IHqMW5x-oWrh8T1ZXhE-9_K8pXMTJ0'
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
+
